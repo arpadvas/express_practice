f4ea4d36e4760a76e43b15b2772f76f48effd02c
Arpad Vas <avas@corp-ad.credorax.com>
2016-12-19 17:13:10 +0100

    templates added

--- app.js
+++ app.js
@@ -13,23 +13,12 @@ app.get("/", function(req, res) {
 	res.render("index");
 });
 
-app.get("/blog/:title?", function(req, res) {
-	var title = req.params.title;
-	if (title === undefined) {
-		res.status(503);
-		res.send("This page is under construction!");
-	} else {
-		var post = posts[title] || {};
-		res.render("post", {post: post});
-	}
-});
-
 app.get("/:city", function(req, res) {
 	var city = req.params.city;
 	request.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=c55d418452d87000440c7941c24c86d7', function(err, response, body) {
 		if (!err && response.statusCode == 200) {
             var locals = JSON.parse(body);
-            res.send(locals);
+		res.render("post", locals);
         }
 	});
 });
