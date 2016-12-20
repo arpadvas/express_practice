5ba90621f9480576790d65d8d76166cdae94cd6e
Arpad Vas <avas@corp-ad.credorax.com>
2016-12-20 16:49:53 +0100

    adding error msg, post function

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var posts = require("./src/mock/posts.json");
 
 var app = express();
 var request = require("request");
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "jade");
 app.set("views", __dirname + "/src/templates");
@@ -13,12 +17,14 @@ app.get("/", function(req, res) {
 	res.render("index");
 });
 
-app.get("/:city", function(req, res) {
-	var city = req.params.city;
+app.post("/city", function(req, res) {
+	var city = req.body.city;
 	request.get('http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&appid=c55d418452d87000440c7941c24c86d7', function(err, response, body) {
+		var locals = JSON.parse(body);
 		if (!err && response.statusCode == 200) {
-            var locals = JSON.parse(body);
-		res.render("post", locals);
+			res.render("post", locals);
+        } else {
+        	res.render("index", locals);
         }
 	});
 });
